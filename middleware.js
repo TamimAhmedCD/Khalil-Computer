677628b012e1fe4a565229e6c8a641a26b643daa
Tamim Ahmed <tamim20072@gmail.com>
2025-05-01 14:41:21 +0600

    fff

--- middleware.js
+++ middleware.js
@@ -2,23 +2,20 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.AUTH_SECRET, // same as in authOptions
-  });
-
-  const { pathname } = request.nextUrl;
-
-  if (
-    !token &&
-    (pathname.startsWith("/admin") || pathname.startsWith("/student"))
-  ) {
-    return NextResponse.redirect(new URL("/auth", request.url));
-  }
-
-  return NextResponse.next();
+  // const token = await getToken({
+  //   req: request,
+  //   secret: process.env.AUTH_SECRET, // same as in authOptions
+  // });
+  // const { pathname } = request.nextUrl;
+  // if (
+  //   !token &&
+  //   (pathname.startsWith("/admin") || pathname.startsWith("/student"))
+  // ) {
+  //   return NextResponse.redirect(new URL("/auth", request.url));
+  // }
+  // return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/student/:path*"],
+  // matcher: ["/admin/:path*", "/student/:path*", "/auth"],
 };
