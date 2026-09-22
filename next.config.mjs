767d2e207742c2dda2ebc07d0b39edaa4eed6652
TamimAhmedCD <tamim20072@gmail.com>
2026-09-22 21:49:27 +0600

    landing page host updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "techgeekbuzz.com",
-      "cdn.10minuteschool.com",
-      "revolutionit.com.bd",
-      "img.freepik.com",
-      "res.cloudinary.com",
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
 };
